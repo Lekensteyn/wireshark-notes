b86878e458d5d7deb21218ce6598b98af4ed7ec2
Peter Wu <peter@lekensteyn.nl>
2018-12-19 17:50:33 +0100

    windows-libs: include missing gcc libs for GnuTLS
    Tested in a clean Win7 SP1 x64 image.

--- windows-libs/make-gnutls-libs-zip.sh
+++ windows-libs/make-gnutls-libs-zip.sh
@@ -12,12 +12,14 @@ umask 22
 # https://src.fedoraproject.org/rpms/mingw-gnutls/blob/f29/f/mingw-gnutls.spec
 # https://apps.fedoraproject.org/packages/mingw64-gnutls
 #
-# parse-fedorarepo.py --ignore gcc --ignore gcc-c++ --ignore crt --ignore pkg-config --release 29 mingw64-gnutls mingw32-gnutls
-# If gcc is not ignored (for libgcc_s_seh-1.dll), do use --ignore binutils --ignore cpp --ignore headers
+# parse-fedorarepo.py --ignore binutils --ignore cpp --ignore headers --ignore gcc-c++ --ignore crt --ignore pkg-config --release 29 mingw64-gnutls mingw32-gnutls --downloadUsing cached file repomd.xml
 #
 # Dependency tree:
 # + mingw64-gnutls                                                 3.6.3-1.fc29.noarch
+#   + mingw64-gcc (mingw64(libgcc_s_seh-1.dll))                    8.2.0-3.fc29.x86_64
+#     + mingw64-winpthreads (mingw64(libwinpthread-1.dll))
 #   + mingw64-gmp (mingw64(libgmp-10.dll))                         6.1.2-4.fc29.noarch
+#     + mingw64-gcc (mingw64(libgcc_s_seh-1.dll))
 #   + mingw64-nettle (mingw64(libhogweed-4.dll))                   3.4-2.fc29.noarch
 #     + mingw64-gmp (mingw64(libgmp-10.dll))
 #   + mingw64-nettle (mingw64(libnettle-6.dll))
@@ -28,21 +30,26 @@ umask 22
 #   + mingw64-winpthreads (mingw64(libwinpthread-1.dll))           5.0.4-2.fc29.noarch
 #   + mingw64-libtasn1
 # + mingw32-gnutls                                                 3.6.3-1.fc29.noarch
+#   + mingw32-gcc (mingw32(libgcc_s_sjlj-1.dll))                   8.2.0-3.fc29.x86_64
+#     + mingw32-winpthreads (mingw32(libwinpthread-1.dll))
 #   + mingw32-gmp (mingw32(libgmp-10.dll))                         6.1.2-4.fc29.noarch
+#     + mingw32-gcc (mingw32(libgcc_s_sjlj-1.dll))
 #   + mingw32-nettle (mingw32(libhogweed-4.dll))                   3.4-2.fc29.noarch
+#     + mingw32-gcc (mingw32(libgcc_s_sjlj-1.dll))
 #     + mingw32-gmp (mingw32(libgmp-10.dll))
 #   + mingw32-nettle (mingw32(libnettle-6.dll))
 #   + mingw32-p11-kit (mingw32(libp11-kit-0.dll))                  0.23.7-5.fc29.noarch
 #     + mingw32-libffi (mingw32(libffi-6.dll))                     3.1-4.fc29.noarch
+#     + mingw32-gcc (mingw32(libgcc_s_sjlj-1.dll))
 #     + mingw32-libtasn1 (mingw32(libtasn1-6.dll))
 #   + mingw32-libtasn1 (mingw32(libtasn1-6.dll))                   4.13-3.fc29.noarch
+#     + mingw32-gcc (mingw32(libgcc_s_sjlj-1.dll))
 #   + mingw32-winpthreads (mingw32(libwinpthread-1.dll))           5.0.4-2.fc29.noarch
 #   + mingw32-libtasn1
-# Skipping this as it should be included with the glib package:
-#   + mingw64-gcc (mingw64(libgcc_s_seh-1.dll))                    8.2.0-3.fc29.x86_64
 #
 urls=(
 https://mirror.nl.leaseweb.net/fedora/linux/releases/29/Everything/x86_64/os/Packages/m/mingw64-gnutls-3.6.3-1.fc29.noarch.rpm
+https://mirror.nl.leaseweb.net/fedora/linux/releases/29/Everything/x86_64/os/Packages/m/mingw64-gcc-8.2.0-3.fc29.x86_64.rpm
 https://mirror.nl.leaseweb.net/fedora/linux/releases/29/Everything/x86_64/os/Packages/m/mingw64-gmp-6.1.2-4.fc29.noarch.rpm
 https://mirror.nl.leaseweb.net/fedora/linux/releases/29/Everything/x86_64/os/Packages/m/mingw64-nettle-3.4-2.fc29.noarch.rpm
 https://mirror.nl.leaseweb.net/fedora/linux/releases/29/Everything/x86_64/os/Packages/m/mingw64-p11-kit-0.23.7-5.fc29.noarch.rpm
@@ -50,6 +57,7 @@ https://mirror.nl.leaseweb.net/fedora/linux/releases/29/Everything/x86_64/os/Pac
 https://mirror.nl.leaseweb.net/fedora/linux/releases/29/Everything/x86_64/os/Packages/m/mingw64-libtasn1-4.13-3.fc29.noarch.rpm
 https://mirror.nl.leaseweb.net/fedora/linux/releases/29/Everything/x86_64/os/Packages/m/mingw64-winpthreads-5.0.4-2.fc29.noarch.rpm
 https://mirror.nl.leaseweb.net/fedora/linux/releases/29/Everything/x86_64/os/Packages/m/mingw32-gnutls-3.6.3-1.fc29.noarch.rpm
+https://mirror.nl.leaseweb.net/fedora/linux/releases/29/Everything/x86_64/os/Packages/m/mingw32-gcc-8.2.0-3.fc29.x86_64.rpm
 https://mirror.nl.leaseweb.net/fedora/linux/releases/29/Everything/x86_64/os/Packages/m/mingw32-gmp-6.1.2-4.fc29.noarch.rpm
 https://mirror.nl.leaseweb.net/fedora/linux/releases/29/Everything/x86_64/os/Packages/m/mingw32-nettle-3.4-2.fc29.noarch.rpm
 https://mirror.nl.leaseweb.net/fedora/linux/releases/29/Everything/x86_64/os/Packages/m/mingw32-p11-kit-0.23.7-5.fc29.noarch.rpm
@@ -80,6 +88,7 @@ for url in "${urls[@]}"; do
 done
 sha256sum --check <<SHA256
 e61e22296b0365334dc60aa67986b845bba5208f1d562093cb53588f1113ed17  mingw64-gnutls-3.6.3-1.fc29.noarch.rpm
+099d43f881e6c7182f20b743d54d0ef96590e6cadad8d37853a5accf9f6898fa  mingw64-gcc-8.2.0-3.fc29.x86_64.rpm
 d0a60418a94ce429f2cdb919cc2730ac123be2259481afc2b857349693c2ad92  mingw64-gmp-6.1.2-4.fc29.noarch.rpm
 48d3811ec95f44825539e4ef7c1290e74c05d1eabbc3fd66e7f60c1f3480a430  mingw64-nettle-3.4-2.fc29.noarch.rpm
 5a11ed1bff0b50a0c97a8a084fe46975307f420ecf800eca42fd572a3fcf0f01  mingw64-p11-kit-0.23.7-5.fc29.noarch.rpm
@@ -87,6 +96,7 @@ bd97d7e514d7f4265f24411cca247b1f1666ef3056a5b9ba0017aeb9d2bb31f0  mingw64-libffi
 6f64aa4dcb2eefed7017396ab4caf3ae22cb06b545b2d33891922226b1d9b228  mingw64-libtasn1-4.13-3.fc29.noarch.rpm
 6044fb0fdd621ed3dc36556ac100f1e74285c5c0ffcf063c99eaa32f37e7e48e  mingw64-winpthreads-5.0.4-2.fc29.noarch.rpm
 80a931b1e629f04fad3407f2f7cf9b7c680f79b4a036b9bee49f127f1f036f5a  mingw32-gnutls-3.6.3-1.fc29.noarch.rpm
+160550a934b2728c284cb1a91d5c9866016231d3818cccae1c7eea3aac5d5198  mingw32-gcc-8.2.0-3.fc29.x86_64.rpm
 b30b64b10faa3d9e910bf6058f7cf60d3fc7c6deca0cafbf60ba4232ba91a983  mingw32-gmp-6.1.2-4.fc29.noarch.rpm
 973803a6583f960cb1ccba08c1f090de94d11ec789cdcb4fba9966c2f4c967e0  mingw32-nettle-3.4-2.fc29.noarch.rpm
 f9b389aee9c28fdd43d5185055bbf096094e3c99710c7db2f92052796ce8aa8a  mingw32-p11-kit-0.23.7-5.fc29.noarch.rpm
